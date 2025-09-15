9450c6c00272fab8f3f3f2fc87dc9efa05e21f0c
PrisD <prisciladellavecchia@gmail.com>
2025-09-14 21:03:43 -0300

    Add ESLint configuration 😱
    Introduced a new eslint.config.js file with recommended rules for JS, TS, and React. Updated package.json and package-lock.json to include ESLint, TypeScript ESLint, eslint-plugin-react, and related dependencies for code linting.

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import { defineConfig } from 'eslint/config';
+
+export default defineConfig([
+    { files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.browser } },
+    tseslint.configs.recommended,
+    pluginReact.configs.flat.recommended,
+    {
+        rules: {
+            'semi': ['error', 'always'],
+            'quotes': ['error', 'single'],
+            'eqeqeq': ['error', 'always'],
+            'no-var': 'error',
+            'prefer-const': 'error',  
+            'no-console': 'warn',
+            'indent': ['error', 4],
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'warn',
+            'react/jsx-pascal-case': 'error'
+        },
+    },
+]);
